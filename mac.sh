66073ba22851b5a28635f162a8f64d7d7d176f8a
Andrew Hite <ahite@getsatisfaction.com>
2015-05-28 10:41:03 -0500

    Remove GS specific installations

--- mac.sh
+++ mac.sh
@@ -27,6 +27,9 @@ fancy_echo "Fixing permissions ..."
   successfully sudo mkdir -p /usr/local
   successfully sudo chown -R `whoami` /usr/local
 
+fancy_echo "Install and set San Francisco as system font ..."
+  successfully ruby -e "$(curl -fsSL https://raw.github.com/wellsriley/YosemiteSanFranciscoFont/master/install)"
+
 fancy_echo "Beginning installation of applications ..."
 
 fancy_echo "Installing Homebrew ..."
@@ -35,170 +38,48 @@ fancy_echo "Installing Homebrew ..."
   successfully brew tap homebrew/dupes
 
 fancy_echo "Installing Homebrew Cask ..."
-  successfully brew tap phinze/homebrew-cask
-  successfully brew install brew-cask
+  successfully brew install caskroom/cask/brew-cask
+  export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
-fancy_echo "Installing GNU Compiler Collection and dependencies ..."
-  successfully brew install autoconf automake apple-gcc42
-  export CC=/usr/local/bin/gcc-4.2
-  export CXX=/usr/local/bin/c++-4.2
-
-fancy_echo "Installing applications ..."
-  successfully brew cask install airmail
-  successfully brew cask install alfred
+fancy_echo "Installing general applications ..."
+  read -p "Please install Airmail via the App Store ..." -n1 -s
+  read -p "Please install Pixelmator via the App Store ..." -n1 -s
+  read -p "Please install Wunderlist via the App Store ..." -n1 -s
   successfully brew cask install divvy
   successfully brew cask install dropbox
-  successfully brew cask install fantastical
-  successfully brew cask install firefox
   successfully brew cask install flux
   successfully brew cask install google-chrome
-  successfully brew cask install google-drive
+  successfully brew cask install intellij-idea
   successfully brew cask install iterm2
-  successfully brew cask install knox
-  successfully brew cask install mou
+  successfully brew cask install java
   successfully brew cask install onepassword
   successfully brew cask install slack
   successfully brew cask install spotify
-  successfully brew cask install the-unarchiver
+  successfully brew cask install spotify-notifications
+  successfully brew cask install skype
   successfully brew cask install virtualbox
-  successfully brew cask install vlc
   successfully brew cask install ynab
   successfully brew cask install zoomus
 
-fancy_echo "Making sure homebrew-cask and Alfred play nice ..."
-  successfully brew alfred
-
 fancy_echo "Beginning installation of development tools ..."
-
-fancy_echo "Creating LaunchAgents directory ..."
-  successfully mkdir -p ~/Library/LaunchAgents
-
-fancy_echo "Installing Ctags ..."
   successfully brew install ctags
-
-fancy_echo "Installing Elastic Search ..."
-  successfully brew install elasticsearch
-  ln -sfv /usr/local/opt/elasticsearch/*.plist ~/Library/LaunchAgents
-  launchctl load ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist
-
-fancy_echo "Installing Git ..."
   successfully brew install git
   successfully brew install git-extras
-
-fancy_echo "Installing GitHub CLI ..."
-  successfully brew install hub
-
-fancy_echo "Installing Grep ..."
-  successfully brew install homebrew/dupes/grep
-
-fancy_echo "Installing ImageMagick ..."
-  successfully brew install imagemagick
-
-fancy_echo "Installing Memcached ..."
-  successfully brew install memcached
-  ln -sfv /usr/local/opt/memcached/*.plist ~/Library/LaunchAgents
-  launchctl load ~/Library/LaunchAgents/homebrew.mxcl.memcached.plist
-
-fancy_echo "Installing MySQL ..."
-  successfully brew install mysql
-  ln -sfv /usr/local/opt/mysql/*.plist ~/Library/LaunchAgents
-  launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
-  successfully unset TMPDIR
-  successfully mysql_install_db --verbose --user=`whoami` --basedir="$(brew --prefix mysql)" --datadir=/usr/local/var/mysql --tmpdir=/tmp
-
-fancy_echo "Installing Nginx ..."
-  successfully brew install nginx
-  ln -sfv /usr/local/opt/nginx/*.plist ~/Library/LaunchAgents
-  launchctl load ~/Library/LaunchAgents/homebrew.mxcl.nginx.plist
-
-fancy_echo "Installing Node.js ..."
-  successfully brew install node
-
-fancy_echo "Installing Postgres ..."
-  successfully brew install postgres
-  successfully initdb /usr/local/var/postgres -E utf8
-  ln -sfv /usr/local/opt/postgres/*.plist ~/Library/LaunchAgents
-  launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
-
-fancy_echo "Installing Rabbit MQ ..."
-  successfully brew install rabbitmq
-  ln -sfv /usr/local/opt/rabbitmq/*.plist ~/Library/LaunchAgents
-  launchctl load ~/Library/LaunchAgents/homebrew.mxcl.rabbitmq.plist
-
-fancy_echo "Installing Python ..."
-  successfully brew install python
-
-fancy_echo "Installing QT ..."
-  successfully brew install qt
-
-fancy_echo "Installing reattach-to-user-namespace ..."
+  successfully brew install grep
   successfully brew install reattach-to-user-namespace
-
-fancy_echo "Installing Redis ..."
-  successfully brew install redis
-  ln -sfv /usr/local/opt/redis/*.plist ~/Library/LaunchAgents
-  launchctl load ~/Library/LaunchAgents/homebrew.mxcl.redis.plist
-
-fancy_echo "Installing The Silver Searcher ..."
   successfully brew install the_silver_searcher
-
-fancy_echo "Installing Tig ..."
   successfully brew install tig
-
-fancy_echo "Installing Tmux ..."
   successfully brew install tmux
-
-fancy_echo "Installing Vim ..."
   successfully brew install vim --with-python --with-ruby --with-perl
-
-fancy_echo "Installing Watch ..."
   successfully brew install watch
-
-fancy_echo "Installing Wemux ..."
   successfully brew install wemux
-
-fancy_echo "Installing Wget ..."
   successfully brew install wget
-
-fancy_echo "Installing Zsh ..."
   successfully brew install zsh
-
-fancy_echo "Installing node.js ..."
-  successfully brew install node
-
-fancy_echo "Installing phantom.js ..."
-  successfully brew install phantomjs
-
-fancy_echo "Installing Powerline ..."
   successfully pip install git+git://github.com/Lokaltog/powerline
   successfully pip install git+git://github.com/xcambar/powerline-segment-battery
 
-fancy_echo "Beginning installation of Ruby environment ..."
-
 fancy_echo "Creating development workspace ..."
   successfully mkdir -p ~/Code
-
-fancy_echo "Installing system libraries recommended for Ruby ..."
-  successfully brew install gdbm libffi libksba libyaml openssl
-
-fancy_echo "Installing RVM ..."
-  successfully curl -L https://get.rvm.io | bash -s stable --ruby
-  successfully source ~/.rvm/scripts/rvm
-
-fancy_echo "Hey RVM - SHHHHHH!"
-  successfully rvm rvmrc warning ignore all.rvmrcs
-
-fancy_echo "Installing ruby 2.0.0 ..."
-  successfully rvm install 2.1 --with-openssl-dir=`brew --prefix openssl` --without-tk --without-tcl
-
-fancy_echo "Setting Ruby 2.0.0 as global default Ruby ..."
-  successfully rvm --default use 2.1
-
-fancy_echo "Update to latest Rubygems version ..."
-  successfully gem update --system
-
-fancy_echo "Installing critical Ruby gems for Rails development ..."
-  successfully gem install bundler rails --no-rdoc --no-ri
 
 fancy_echo "Installing dotfiles ..."
   successfully git clone git://github.com/andyhite/dotfiles.git ~/.dotfiles
